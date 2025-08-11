f5f18ce689bf99c7a87823eb57f950375e207b3f
HÔKWÈ Holding <hokweholding@gmail.com>
2025-08-11 13:34:11 +0200

    Fix(Auths) - Envoi du lien d'activation ainsi que des identifiants de connexion

--- app/Services/EvaluationService.php
+++ app/Services/EvaluationService.php
@@ -765,6 +765,9 @@ class EvaluationService extends BaseService implements EvaluationServiceInterfac
                 if ((auth()->user()->profilable_type == Organisation::class || auth()->user()->profilable_type == Dpaf::class) && auth()->user()->profilable?->ministere && $ideeProjet->ministere && (auth()->user()->profilable->ministere?->id == $ideeProjet->ministere->id)) {
 
                     $isAssigned = $evaluation->evaluateursClimatique()->where("id", auth()->id())->first()?->hasPermissionTo('effectuer-evaluation-climatique-idee-projet');
+
+                    throw new Exception("Error Processing Request" . json_encode($evaluation->evaluateursClimatique()->get()), 403);
+
                     //->get()->filter(fn($user) => $user->hasPermissionTo('effectuer-evaluation-climatique-idee-projet'))->isNotEmpty(); // ✅ on vérifie bien si la collection n'est pas vide;
                     $evaluateur = $evaluation->evaluateursClimatique()->where("id", auth()->id())
                         ->get()->filter(fn($user) => $user?->hasPermissionTo('effectuer-evaluation-climatique-idee-projet'))->isNotEmpty(); // ✅ on vérifie bien si la collection n'est pas vide;
