67e942a3c48e500a23d61b2fe7d3c8d2ca78521d
HÔKWÈ Holding <hokweholding@gmail.com>
2025-09-05 10:25:36 +0200

    Commit

--- app/Services/IdeeProjetService.php
+++ app/Services/IdeeProjetService.php
@@ -116,7 +116,7 @@ class IdeeProjetService extends BaseService implements IdeeProjetServiceInterfac
             // Créer ou récupérer l'idée de projet
             $idee = $this->getOrCreateIdeeProjet($data);
 
-            if (auth()->user()->profilable->ministere->id !== $idee->ministere->id && auth()->user()->id != $idee->responsable?->id) {
+            if (auth()->user()->profilable->ministere->id !== $idee->ministere->id) {
                 throw new Exception("Vous n'avez pas les droits d'acces pour effectuer cette action", 403);
             }
 
