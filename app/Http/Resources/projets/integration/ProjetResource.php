16792588522935200691eb99395fe2797632e41a
HÔKWÈ Holding <hokweholding@gmail.com>
2025-09-17 13:46:43 +0200

    API Resource integration

---
+++ app/Http/Resources/projets/integration/ProjetResource.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace App\Http\Resources\projets\integration;
+
+use App\Http\Resources\BaseApiResource;
+use App\Http\Resources\CibleResource;
+use App\Http\Resources\FinancementResource;
+use App\Http\Resources\idees_projet\IdeeProjetResource;
+use App\Http\Resources\LieuInterventionResource;
+use App\Http\Resources\NoteConceptuelleResource;
+use App\Http\Resources\OddResource;
+use App\Http\Resources\RapportResource;
+use App\Http\Resources\SecteurResourcePublic;
+use App\Http\Resources\TdrResource;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ProjetResource extends BaseApiResource
+{
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            // Identifiants et métadonnées
+            'identifiant_bip' => $this->identifiant_bip,
+            'identifiant_sigfp' => $this->identifiant_sigfp,
+            'sigle' => $this->sigle,
+            'titre_projet' => $this->titre_projet,
+
+            'est_a_haut_risque' => $this->est_a_haut_risque,
+            'est_dur' => $this->est_dur,
+            'est_ancien' => $this->est_ancien,
+            'info_etude_prefaisabilite' => $this->info_etude_prefaisabilite,
+            'info_etude_faisabilite' => $this->info_etude_faisabilite,
+
+            // Statuts et phases
+            'statut' => $this->statut?->value ?? $this->statut,
+            'phase' => $this->phase?->value ?? $this->phase,
+            'sous_phase' => $this->sous_phase?->value ?? $this->sous_phase,
+            'type_projet' => $this->type_projet?->value ?? $this->type_projet,
+
+            // Descriptions et contenus principaux
+            'description' => $this->description,
+            'description_projet' => $this->description_projet,
+            'objectif_general' => $this->objectif_general,
+            'situation_actuelle' => $this->situation_actuelle,
+            'situation_desiree' => $this->situation_desiree,
+            'contraintes' => $this->contraintes,
+            'origine' => $this->origine,
+            'fondement' => $this->fondement,
+
+            // Détails techniques et organisationnels
+            'caracteristiques' => $this->caracteristiques,
+            'aspect_organisationnel' => $this->aspect_organisationnel,
+            'description_extrants' => $this->description_extrants,
+            'impact_environnement' => $this->impact_environnement,
+            'echeancier' => $this->echeancier,
+            'duree' => $this->duree,
+
+            // Risques et conclusions
+            'risques_immediats' => $this->risques_immediats,
+            'constats_majeurs' => $this->constats_majeurs,
+            'conclusions' => $this->conclusions,
+            'sommaire' => $this->sommaire,
+            'public_cible' => $this->public_cible,
+
+            // Estimation des coûts
+            'estimation_couts' => $this->estimation_couts,
+            'cout_dollar_americain' => $this->cout_dollar_americain,
+            'cout_dollar_canadien' => $this->cout_dollar_canadien,
+            'cout_euro' => $this->cout_euro,
+
+            // Scores d'évaluation
+            'score_climatique' => $this->score_climatique,
+            'score_amc' => $this->score_amc,
+
+            // Dates importantes
+            'date_debut_etude' => $this->date_debut_etude,
+            'date_fin_etude' => $this->date_fin_etude,
+            'date_prevue_demarrage' => $this->date_prevue_demarrage,
+            'date_effective_demarrage' => $this->date_effective_demarrage,
+
+            // Données JSON structurées
+            'decision' => $this->decision ?? [],
+            'cout_estimatif_projet' => $this->cout_estimatif_projet ?? [],
+            'ficheIdee' =>  $this->ficheIdee ?? [],
+            'parties_prenantes' => $this->parties_prenantes ?? [],
+            'objectifs_specifiques' => $this->objectifs_specifiques ?? [],
+            'resultats_attendus' => $this->resultats_attendus ?? [],
+            'body_projet' => $this->body_projet ?? [],
+            'description_decision' => $this->description_decision,
+
+            'champs' => $this->champs->map(function ($champ) {
+                return [
+                    'id' => $champ->id,
+                    'attribut' => $champ->attribut,
+                    'value' => $champ->pivot->valeur
+                ];
+            }),
+            // Relations principales (loaded when needed)
+            'secteur' => $this->whenLoaded('secteur', new SecteurResourcePublic($this->secteur)),
+            'ministere' => $this->whenLoaded('ministere'),
+            'categorie' => $this->whenLoaded('categorie'),
+            'responsable' => $this->whenLoaded('responsable'),
+            'demandeur' => $this->whenLoaded('demandeur'),
+            'porteur_projet' => $this->porteur_projet,
+            'ideeProjet' => new IdeeProjetResource($this->ideeProjet),
+
+            'noteConceptuelle' => $this->whenLoaded('noteConceptuelle', function() {
+                return new NoteConceptuelleResource($this->noteConceptuelle);
+            }),
+
+            // TDRs
+            'tdr_prefaisabilite' => $this->whenLoaded('tdrPrefaisabilite', function() {
+                return $this->tdrPrefaisabilite->first() ? new TdrResource($this->tdrPrefaisabilite->first()) : null;
+            }),
+            'tdr_faisabilite' => $this->whenLoaded('tdrFaisabilite', function() {
+                return $this->tdrFaisabilite->first() ? new TdrResource($this->tdrFaisabilite->first()) : null;
+            }),
+
+
+            // Rapports etudes (prefaisabilite et faisabilite)
+            'rapport_prefaisabilite' => $this->whenLoaded('rapportPrefaisabilite', function() {
+                return $this->rapportPrefaisabilite->first() ? new RapportResource($this->rapportPrefaisabilite->first()) : null;
+            }),
+
+            'rapport_faisabilite' => $this->whenLoaded('rapportFaisabilite', function() {
+                return $this->rapportFaisabilite->first() ? new TdrResource($this->rapportFaisabilite->first()) : null;
+            }),
+
+            'rapport_evaluation_ex_ante' => $this->whenLoaded('rapportEvaluationExAnte', function() {
+                return $this->rapportEvaluationExAnte->first() ? new TdrResource($this->rapportEvaluationExAnte->first()) : null;
+            }),
+
+            'cibles' => $this->whenLoaded('cibles', CibleResource::collection($this->cibles)),
+            'odds' => $this->whenLoaded('odds', OddResource::collection($this->odds)),
+
+            'sources_de_financement' => $this->whenLoaded('sources_de_financement', FinancementResource::collection($this->sources_de_financement)),
+
+            'composants' => $this->composants->map(function ($composant) {
+                    return [
+                        'id' => $composant->id,
+                        'intitule' => $composant->intitule,
+                        'type_programme' => $composant->typeProgramme->id ?? null
+                    ];
+            }),
+
+            'lieux_intervention' => LieuInterventionResource::collection($this->lieuxIntervention),
+
+            'types_intervention' => $this->whenLoaded('typesIntervention', function () {
+                return $this->typesIntervention->map(function ($type) {
+                    return [
+                        'id' => $type->id,
+                        'nom' => $type->nom
+                    ];
+                });
+            }),
+
+            /*'tdrs_prefaisabilite' => [
+                "document" => $this->tdrs_prefaisabilite,
+                "resume" => $this->resume_tdr_prefaisabilite
+            ],
+
+            'tdrs_faisabilite' => [
+                "document" => $this->tdrs_faisabilite,
+                "resume" => $this->resume_tdr_faisabilite
+            ],
+
+            'rapports_prefaisabilite' => [
+                "document" => $this->rapports_prefaisabilite,
+                "cabinet_etude" => $this->info_cabinet_etude_faisabilite
+            ],
+
+            'rapports_faisabilite' => [
+                "document" => $this->rapports_faisabilite,
+                "cabinet_etude" => $this->info_cabinet_etude_faisabilite
+            ],
+
+            'rapports_prefaisabilite' => [
+                "document" => $this->rapports_prefaisabilite,
+                "cabinet_etude" => $this->info_cabinet_etude_faisabilite
+            ],
+
+            'rapports_evaluation_ex_ante' => [
+                "rapports_evaluation_ex_ante" => $this->rapports_evaluation_ex_ante,
+                "documents_annexe_rapports_evaluation_ex_ante" => $this->documents_annexe_rapports_evaluation_ex_ante
+            ],*/
+
+            // Timestamps
+            'created_at' => Carbon::parse($this->created_at)->format("Y-m-d H:i:s"),
+            'updated_at' => Carbon::parse($this->updated_at)->format("Y-m-d H:i:s"),
+        ];
+    }
+
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function with(Request $request): array
+    {
+        return array_merge(parent::with($request), [
+            'meta' => [
+                'type' => 'ideeprojet',
+                'version' => '1.0',
+            ],
+        ]);
+    }
+}
---
+++ app/Http/Resources/projets/integration/ProjetsResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Resources\projets\integration;
+
+use App\Http\Resources\BaseApiResource;
+use App\Http\Resources\idees_projet\IdeeProjetResource;
+use App\Http\Resources\NoteConceptuelleResource;
+use App\Http\Resources\SecteurResourcePublic;
+use App\Http\Resources\TdrResource;
+use Illuminate\Http\Request;
+
+class ProjetsResource extends BaseApiResource
+{
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            // Identifiants et métadonnées
+            'identifiant_bip' => $this->identifiant_bip,
+            'identifiant_sigfp' => $this->identifiant_sigfp,
+            'sigle' => $this->sigle,
+            'titre_projet' => $this->titre_projet,
+            'porteur_projet' => $this->porteur_projet,
+            'est_a_haut_risque' => $this->est_a_haut_risque,
+            'est_dur' => $this->est_dur,
+            'est_ancien' => $this->est_ancien,
+            'info_etude_prefaisabilite' => $this->info_etude_prefaisabilite,
+            'info_etude_faisabilite' => $this->info_etude_faisabilite,
+
+            // Statuts et phases
+            'statut' => $this->statut?->value ?? $this->statut,
+            'phase' => $this->phase?->value ?? $this->phase,
+            'sous_phase' => $this->sous_phase?->value ?? $this->sous_phase,
+            'type_projet' => $this->type_projet?->value ?? $this->type_projet,
+
+            // Scores d'évaluation
+            'score_climatique' => $this->score_climatique,
+            'score_amc' => $this->score_amc,
+
+            // Descriptions et contenus principaux
+            'description_projet' => $this->description_projet,
+
+            // Détails techniques et organisationnels
+            'duree' => $this->duree,
+            // Données JSON structurées
+            'cout_estimatif_projet' => $this->cout_estimatif_projet ?? [],
+
+            'ideeProjet' =>  $this->whenLoaded('ideeProjet',new IdeeProjetResource($this->ideeProjet)),
+
+            'secteur' => new SecteurResourcePublic($this->secteur),
+
+            'noteConceptuelle' => $this->whenLoaded('noteConceptuelle', function() {
+                return new NoteConceptuelleResource($this->noteConceptuelle);
+            }),
+
+            // TDRs
+            'tdr_prefaisabilite' => $this->whenLoaded('tdrPrefaisabilite', function() {
+                return $this->tdrPrefaisabilite->first() ? new TdrResource($this->tdrPrefaisabilite->first()) : null;
+            }),
+
+            'tdr_faisabilite' => $this->whenLoaded('tdrFaisabilite', function() {
+                return $this->tdrFaisabilite->first() ? new TdrResource($this->tdrFaisabilite->first()) : null;
+            })
+        ];
+    }
+
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function with(Request $request): array
+    {
+        return array_merge(parent::with($request), [
+            'meta' => [
+                'type' => 'projet',
+                'version' => '1.0',
+            ],
+        ]);
+    }
+}
