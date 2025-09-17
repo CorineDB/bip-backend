91640439ac05421f8691b880118638f0a883301f
HÔKWÈ Holding <hokweholding@gmail.com>
2025-09-17 13:45:42 +0200

    Controller

---
+++ app/Http/Controllers/API/Json/IntegrationBip/IntegrationController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API\Json\IntegrationBip;
+
+use App\Http\Controllers\Controller;
+use App\Services\Contracts\IntegrationBipServiceInterface;
+use Illuminate\Http\JsonResponse;
+
+class IntegrationController extends Controller
+{
+    protected IntegrationBipServiceInterface $service;
+
+    public function __construct(IntegrationBipServiceInterface $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(): JsonResponse
+    {
+        return $this->service->getProjetsArrivesAMaturite();
+    }
+}
