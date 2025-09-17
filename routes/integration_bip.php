9792f2282ccdc3e6d6e32851d03b8097c32c8f45
HÔKWÈ Holding <hokweholding@gmail.com>
2025-09-17 15:49:07 +0200

    commit

--- routes/integration_bip.php
+++ routes/integration_bip.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\API\Json\IntegrationBip\IntegrationController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('integration-bip')->middleware(['auth.client', 'scopes:integration-bip'])->group(function () {
+Route::prefix('integration-bip')->middleware(['auth.client', 'scope:integration-bip'])->group(function () {
 
     Route::middleware([/* 'scopes:sync-sigfp' */])->group(function () {
         Route::get('/projets-mature', [IntegrationController::class, 'index'])
