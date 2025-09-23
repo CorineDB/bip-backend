129af044301db2a71b904bf159ef454c97a2ea31
HÔKWÈ Holding <hokweholding@gmail.com>
2025-09-23 18:07:40 +0200

    Commit

--- app/Services/TdrPrefaisabiliteService.php
+++ app/Services/TdrPrefaisabiliteService.php
@@ -1123,7 +1123,7 @@ class TdrPrefaisabiliteService extends BaseService implements TdrPrefaisabiliteS
                             }
                         }
 
-                        throw new Exception("Error Processing Request : " . $projet->info_etude_prefaisabilite);
+                        throw new Exception("Error Processing Request : " . $projet->info_etude_prefaisabilite['est_finance'], 1);
 
 
                         // Toutes les validations sont passées, on peut enregistrer les informations
