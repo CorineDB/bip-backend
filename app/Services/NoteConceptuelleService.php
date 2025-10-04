f7dc4fa5eae9a716eaa9fb9542da52f769a67d84
HÔKWÈ Holding <hokweholding@gmail.com>
2025-10-04 13:58:53 +0200

    Commit

--- app/Services/NoteConceptuelleService.php
+++ app/Services/NoteConceptuelleService.php
@@ -1399,6 +1399,8 @@ class NoteConceptuelleService extends BaseService implements NoteConceptuelleSer
                         'numero_contrat' => null,
                         'numero_dossier' => null,
                         'accept_term' => false,
+                        'rediger_par' =>  $noteConceptuelle->rediger_par,
+                        'updated_at' => null,
                         'rediger_par' => null,
                         'canevas_redaction_note_conceptuelle' => [],
                         'canevas_appreciation_note_conceptuelle' => [],
