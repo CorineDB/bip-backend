95da35afde7c6cbfa1da8aae21beb79d9ef9c6d5
HÔKWÈ Holding <hokweholding@gmail.com>
2025-10-10 13:13:30 +0200

    commit

--- app/Http/Resources/TdrResource.php
+++ app/Http/Resources/TdrResource.php
@@ -58,11 +58,12 @@ class TdrResource extends BaseApiResource
             'evaluateur' => $this->whenLoaded('evaluateur', function(){
                 return new UserResource($this->evaluateur);
             }),
-            'historique_des_tdrs_faisabilite' =>  $this->historique_des_tdrs_faisabilite,
-            'historique_des_tdrs_faisabilite' =>  $this->historique_des_tdrs_faisabilite,
-            'historique_des_tdrs_prefaisabilite' =>  $this->historique_des_tdrs_prefaisabilite/* $this->whenLoaded("historique_des_tdrs_prefaisabilite", function(){
+            'historique_des_tdrs' =>  $this->type == 'faisabilite' ? $this->historique_des_tdrs_faisabilite : $this->historique_des_tdrs_prefaisabilite,
+            "historique_des_evaluations_tdrs" => $this->type == 'faisabilite' ? $this->historique_des_evaluations_tdrs_faisabilite :  $this->historique_des_evaluations_tdrs_prefaisabilite,
+
+            /*'historique_des_tdrs_prefaisabilite' =>  $this->historique_des_tdrs_prefaisabilite/* $this->whenLoaded("historique_des_tdrs_prefaisabilite", function(){
                 return $this->historique_des_tdrs_prefaisabilite;
-            }) */,
+            }),
             "historique_des_evaluations_tdrs_prefaisabilite" => $this->historique_des_evaluations_tdrs_prefaisabilite /* $this->whenLoaded("historique_des_evaluations_tdrs_prefaisabilite", function(){
                 $this->historique_des_evaluations_tdrs_prefaisabilite->pluck("evaluations")->collapse()->map(function($evaluation){
                     return [
@@ -78,7 +79,7 @@ class TdrResource extends BaseApiResource
                         'statut' => $evaluation->statut
                     ];
                 });
-            }) */,
+            }),*/
 
             // Fichiers par type
             'fichier_tdr' => $this->whenLoaded('fichiers', function() {
