69950e19e1f105bba9942e73fa704e3dcc4e5f2d
HÔKWÈ Holding <hokweholding@gmail.com>
2025-10-11 22:01:06 +0200

    Commit

--- app/Models/IdeeProjet.php
+++ app/Models/IdeeProjet.php
@@ -393,8 +393,8 @@ class IdeeProjet extends Model
         /* return $this->morphOne(Evaluation::class, 'projetable')->where("type", "pertinence")
                 ->orderBy('created_at', 'desc')
                 ->first(); */
-        return $this->morphOne(Evaluation::class, 'projetable')
-            ->where('type', 'pertinence')/*
+        return $this->morphMany(Evaluation::class, 'projetable')
+            ->where('type_evaluation', 'pertinence')/*
             ->latestOfMany() */; // ✅ équivalent à orderBy('created_at', 'desc')->first()
 
     }
