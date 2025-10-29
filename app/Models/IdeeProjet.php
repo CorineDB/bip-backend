5fdec2df2048c1f12286924b1c0030473a967954
HÔKWÈ Holding <hokweholding@gmail.com>
2025-10-29 14:40:57 +0100

    Commit

--- app/Models/IdeeProjet.php
+++ app/Models/IdeeProjet.php
@@ -460,9 +460,9 @@ class IdeeProjet extends Model
             'orientations_strategiques' => 'orientations_strategique_png',
             'resultats_strategiques' => 'resultats_strategique_png',
             'objectifs_strategiques' => 'objectifs_strategique_png',
+            'piliers_pag'=>'piliers_pag',
             'axes_pag'=>'axes_pag',
             'actions_pag'=>'actions_pag',
-            'piliers_pag'=>'piliers_pag',
 
             'cibles' => 'cibles',
             'odds' => 'odds',
